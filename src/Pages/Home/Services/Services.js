db2c481bdac5e0c7629a727035cfd3b979fb7c40
wdminarul <wdminarul100@gmail.com>
2021-10-20 22:39:29 +0600

    service-cahnge

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -13,6 +13,7 @@ const Services = () => {
         <div className="mt-5 mb-5 pb-3 services-main" id="services">
             <h2 className="text-center text-success">Services</h2>
             <div className="service-container container">
+                {/* pas data in service component  with map */}
             {
                 services?.map(service=><Service key={service.keys} service={service}></Service>)
             }
